71df29ada58a5317ed87eec12ada27f5369214ed
codxmayank <mayankp334@gmail.com>
2026-02-14 19:31:50 +0530

    update navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,21 +2,30 @@ import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="flex items-end gap-4 mb-10 bg-blue-900 rounded-lg p-6">
+    <div className="flex items-end gap-4 mb-10 border-2 border-gray-500 rounded-lg p-6">
       <NavLink to="/">
         <div className="text-2xl font-bold mr-10">Project</div>
       </NavLink>
-      <NavLink to="/home" className={e => e.isActive ? "text-green-500!" : ""}>
+      <NavLink
+        to="/home"
+        className={(e) => (e.isActive ? "text-green-500!" : "")}
+      >
         <h6>Home</h6>
       </NavLink>
-      <NavLink to="/contact" className={e => e.isActive ? "text-green-500!" : ""}>
+      <NavLink
+        to="/contact"
+        className={(e) => (e.isActive ? "text-green-500!" : "")}
+      >
         <h6>Contact</h6>
       </NavLink>
-      <NavLink to="/about" className={e => e.isActive ? "text-green-500!" : ""}>
+      <NavLink
+        to="/about"
+        className={(e) => (e.isActive ? "text-green-500!" : "")}
+      >
         <h6>About</h6>
       </NavLink>
     </div>
-  )
+  );
 }
 
 export default Navbar;
